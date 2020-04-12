0d667541f4e950e32462780c480a75a232e3b22f
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-04-12 19:28:27 +0530

    added function get_closest_food

--- point.c
+++ point.c
@@ -1,10 +1,23 @@
+#include<stdio.h>
 #include<math.h>
 #include "point.h"
 
-unsigned long int find_distance(POINT point1, POINT point2) {
+DISTANCE find_distance(POINT point1, POINT point2) {
   int sqr_of_dif_of_x = (point2.x - point1.x) * (point2.x - point1.x);
   int sqr_of_dif_of_y = (point2.y - point1.y) * (point2.y - point1.y);
   return sqrt(sqr_of_dif_of_x + sqr_of_dif_of_y);
 }
 
-void get_closest_food(POINT food_points[], int points_length, POINT current_location, POINT *closest_food_location) {}
+
+void get_closest_food(POINT food_points[], int points_length, POINT current_location, POINT *closest_food_location) {
+  DISTANCE shortest_distance = find_distance(current_location, food_points[0]);
+  *closest_food_location = food_points[0];
+  
+  for (int index = 0; index < points_length; index++) {
+    DISTANCE current_distance = find_distance(current_location, food_points[index]);
+    if (current_distance < shortest_distance) {
+      shortest_distance = current_distance;
+      *closest_food_location = food_points[index];
+    }
+  }
+}
